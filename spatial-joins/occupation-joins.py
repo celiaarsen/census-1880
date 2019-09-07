80c622fd5da9645828888617a981cc12b18e2065
Celia Arsen <cla2143@barnard.edu>
2019-09-07 16:44:01 -0400

    outline for occupation script

---
+++ spatial-joins/occupation-joins.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  7 16:23:26 2019
+
+@author: Celia Arsen
+
+-Goal of this script will be to join the number of people from certain occupational 
+categories to city blocks
+-Inputs should be something like:
+    AlbanyCE.shp (contains field occlabelB with occupations) and
+    AlbanyET.shp (cotains field popDense with population density)
+-Eventually we want this to walk to through a directory and do it for every file in the directory
+
+"""
+
