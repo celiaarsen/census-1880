1c023d7db28d7747a5690e72368b2bdf58b07cd0
Celia Arsen <cla2143@barnard.edu>
2019-09-07 17:04:11 -0400

    create script for occ curves

---
+++ density-curves/occupation-density-curves.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  7 17:01:59 2019
+
+@author: Celia
+
+Pre-processing steps:
+    1. Make composite-city from PEOPLE, not buildings
+    2. Make copies of dbf files as csv
+    
+"""
+
