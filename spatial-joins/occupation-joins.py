00cd1789c11c66d45831f5bb618a780476f1e2e2
Celia Arsen <cla2143@barnard.edu>
2019-09-07 17:37:00 -0400

    described steps for spatial join

--- spatial-joins/occupation-joins.py
+++ spatial-joins/occupation-joins.py
@@ -10,6 +10,23 @@ categories to city blocks
     AlbanyCE.shp (contains field occlabelB with occupations) and
     AlbanyET.shp (cotains field popDense with population density)
 -Eventually we want this to walk to through a directory and do it for every file in the directory
-
+    
+Pre-processing:
+    1. May want to exclude beforehand certain people from AlbanyCE.shp
+    
+Steps:    
+    1. Read in AlbanyET.shp as target features
+    2. Read in AlbanyCE.shp
+    3. Create a (temporary) copy of AlbanyCE.shp called AlbanyCE_occs.shp
+    4. Create dummy variable categories in AlbanyCE_occs.shp for any occupations of interest
+    5. Set AlbanyCE_occs.shp as join features
+    3. Set destination for output files (will want to be set dynamically, eventually)
+        -Something like AlbanyOcc.shp
+    4. Create field mapping objects
+    5. Set merge rules:
+        -Sum all dummy variables from AlbanyCE_occs.shp
+        -First for all variables of interest in AlbanyET.shp
+        -Remove any other variables that are not necessary
+    5. Perform spatial join
 """
 
