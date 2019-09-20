daa41b4b3ef92c73b75b53170298b7b4bd2b341a
Celia Arsen <cla2143@barnard.edu>
2019-09-20 16:07:40 -0400

    fix typos

--- scripts/spatial-joins/occupation-joins.py
+++ scripts/spatial-joins/occupation-joins.py
@@ -8,7 +8,7 @@ Created on Sat Sep  7 16:23:26 2019
 categories to city blocks
 -Inputs should be something like:
     AlbanyCE.shp (contains field occlabelB with occupations) and
-    AlbanyET.shp (cotains field popDense with population density)
+    AlbanyET.shp (contains field popDense with population density)
 -Eventually we want this to walk to through a directory and do it for every file in the directory
     
 Pre-processing:
