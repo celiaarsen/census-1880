213468de7c91d32c3ba11d2adcfcafa517af9127
Celia Arsen <cla2143@barnard.edu>
2019-10-04 15:10:20 -0400

    join 1CE to 1ET

--- scripts/spatial-joins/occupation-joins.py
+++ scripts/spatial-joins/occupation-joins.py
@@ -4,11 +4,11 @@ Created on Sat Sep  7 16:23:26 2019
 
 @author: Celia Arsen
 
--Goal of this script will be to join the number of people from certain occupational 
-categories to city blocks
+-Goal of this script will be to join the number of people from certain occupations 
+to city blocks
 -Inputs should be something like:
     AlbanyCE.shp (contains field occlabelB with occupations) and
-    AlbanyET.shp (contains field popDense with population density)
+    AlbanyET.shp (cotains field popDense with population density)
 -Eventually we want this to walk to through a directory and do it for every file in the directory
     
 Pre-processing:
@@ -28,11 +28,68 @@ Steps:
         -First for all variables of interest in AlbanyET.shp
         -Remove any other variables that are not necessary
     5. Perform spatial join
+    
+    Lines that are commented out with three single quotes should be uncommented before execution
 """
 print("script started")
 import arcpy
+
+
+arcpy.env.overwriteOutput = 1
 print("arcpy imported")
-directory = "C:/Users/Celia/Desktop/1880DataByCity - Copy/Albany"
-targetFeatures = directory+"AlbanyET.shp"
-individualPeople = directory+"AlbanyCE.shp"
+directory = "C:/Users/Celia/Desktop/1880DataByCity-Copy/Albany/"
+target_features = directory+"AlbanyET.shp"
+join_features = 'RESET AFTER COPYING TARGET FEATURES'
+individual_people = directory+"AlbanyCE.shp"
+out_features = directory+"AlbanyOcc.shp"
+print('target features: ', target_features)
+print('full count census: ', individual_people)
+
+
+#CopyFeatures(in_features, out_feature_class)
+#arcpy.CopyFeatures_management(individual_people, directory+"AlbanyCE_occs.shp")
+#make the copy of AlbanyCE the join features
+join_features = directory+"AlbanyCE_occs.shp"
+print("Copied CE file")
+print('join features: ', join_features)
+field_name = "occs"
+
+codeblock = """
+def occ_dummy(occupation):
+    if (occupation=="LABORER"):
+        return 1
+    else:
+        return 0 """
+
+#AddField(in_table, field_name, field_type, {field_precision}, {field_scale})
+'''arcpy.AddField_management(joinFeatures, field_name, "SHORT")'''
+#CalculateField(in_table, field, expression, {expression_type}, {code_block})
+#calculate a dummy variable for the attribute occs
+'''arcpy.CalculateField_management(join_features, field_name, "occ_dummy(!occlabelB!)", "PYTHON3", codeblock)'''
+
+#initialize a FieldMappings object with the target and join features
+fieldMappings = arcpy.FieldMappings()
+fieldMappings.addTable(target_features)
+fieldMappings.addTable(join_features)
+
+#Set merge rules
+#Rule for occs should be "Sum", so we get the count of people with that occupation on the block
+occsIndex = fieldMappings.findFieldMapIndex("occs")
+occsFieldMap = fieldMappings.getFieldMap(occsIndex)
+occsFieldMap.mergeRule = "Sum"
+fieldMappings.replaceFieldMap(occsIndex, occsFieldMap)
+
+#We don't need to join any of the other fields from the Census to the blocks, so remove those 
+#fieldMaps from the fieldMappings
+for field in arcpy.ListFields(join_features):
+    if ((field.name != 'occs') and not(field.required)):
+        #remove all other non-required fields in the joinFeatures table
+        x = fieldMappings.findFieldMapIndex(field.name)
+        fieldMappings.removeFieldMap(x)
+
+#perform the spatial join
+arcpy.SpatialJoin_analysis(target_features, join_features, out_features,"JOIN_ONE_TO_ONE", "KEEP_ALL", fieldMappings, "COMPLETELY_CONTAINS")
+
+print('finished!')
+    
 
